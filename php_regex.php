54e2fe1bcb649ca647e9592cef087bc85bc43009
sebastian cordoba <cbascordoba14@gmail.com>
2019-10-01 11:30:16 -0500

    A traves de la expresion regular se encuentran todos los juegos llevados a cabo en enero de 2018

--- php_regex.php
+++ php_regex.php
@@ -2,8 +2,25 @@
 
 $file = fopen("resultados.csv" , "r");
 
+$match =0;
+$nomatch = 0;
 while(!feof($file)){
   $line = fgets($file);
-  echo $line;
+
+  // Encontrar todos los juegos de enero de 2018
+  if(preg_match(
+    '/^2018\-01\-(\d\d),.*$/',
+    $line,
+    $m
+  )){
+    $match++;
+    print_r($m);
+  
+  }else{
+    $nomatch++;
+  }
 }
 fclose($file);
+
+//estadistica final que me permite saber cuantos match hay 
+printf("\n\nmatch: %d\n no match: %d\n",$match,$nomatch);
