9ba4ed02058c6615d2cbc787dba3f89d23578817
sebastian cordoba <cbascordoba14@gmail.com>
2019-10-01 15:01:27 -0500

    Se indica si ganó (visitante o local) o fue empate a través de expresiones regulares al consultar un archivo csv

--- php_regex.php
+++ php_regex.php
@@ -7,14 +7,25 @@ $nomatch = 0;
 while(!feof($file)){
   $line = fgets($file);
 
-  // Encontrar todos los juegos de enero de 2018
+  // Encontrar todo los resultados en la historia 
+  // 2018-01-30,Jamaica,Korea Republic,2,2,Friendly,Antalya,Turkey,TRUE
+  // local, visitante, marcador local, marcador visitante
+
   if(preg_match(
-    '/^2018\-01\-(\d\d),.*$/',
+    '/^(\d{4}\-\d\d\-\d\d),(.+),(.+),(\d+),(\d+),.*$/i',
     $line,
     $m
   )){
+    if($m[4]==$m[5]){
+      echo "Empate:";
+    }else if($m[4]>$m[5]){
+      echo "Ganó Local:";
+    }
+    else{
+      echo "Ganó Visitante: ";
+    }
     $match++;
-    print_r($m);
+    printf("\t%s vs %s [%d-%d]\n", $m[2],$m[3],$m[4], $m[5]);
   
   }else{
     $nomatch++;
